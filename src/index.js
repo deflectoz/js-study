1f86bfb94247f0a0626711246094b64ac77fa70c
Muhammad Febriyan Akbar <muh.febrian@gmail.com>
2023-08-29 22:54:31 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// function recrusiveFunct(n) {
+//   if (n === 0) {
+//     return;
+//   }
+// console.log(n)
+//   recrusiveFunct(n - 1);
+// }
+
+// recrusiveFunct(10);
+
+//Recrusive
+
+const name = (name) => {
+  return `Hello ${name}`;
+};
+
+alert(name("Paul"));
